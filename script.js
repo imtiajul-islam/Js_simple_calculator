7947c51404183732ee6b0d74614624f2dab85f44
imtiajul-islam <imtiajul38@gmail.com>
2022-07-05 09:24:01 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let screen = document.querySelector("#screen");
+let buttons = document.querySelectorAll("button");
+let screenValue = "";
+
+for (let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', (e) => {
+        let buttonText = e.target.innerText;
+        if (buttonText == "x") {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        } else if (buttonText == 'c') {
+            screenValue = '';
+            screen.value = screenValue;
+        } else if (buttonText == '=') {
+            screenValue = eval(screenValue);
+            screen.value = screenValue;
+        } else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+    })
+}
